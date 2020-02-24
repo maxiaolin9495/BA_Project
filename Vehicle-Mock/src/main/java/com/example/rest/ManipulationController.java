37646c6e418b76cbe14794a6f45b88909a395147
Xiaolin Ma <xiaolin.ma@ic-consult.com>
2020-02-24 14:14:23 +0100

    add test controller

--- Vehicle-Mock/src/main/java/com/example/rest/ManipulationController.java
+++ Vehicle-Mock/src/main/java/com/example/rest/ManipulationController.java
@@ -2,33 +2,35 @@ package com.example.rest;
 
 import com.example.data.CertificateResponse;
 import com.example.services.certificate.CertificateService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.security.cert.X509Certificate;
-import java.util.Base64;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ManipulationController {
+
+    Logger log = LoggerFactory.getLogger(ManipulationController.class);
+    @Value("${rootCAs.id}")
+    private String[] caIds;
 
     @Autowired
     CertificateService certificateService;
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, path = "/manipulation")
-    public ResponseEntity<CertificateResponse> requestLTC(@RequestHeader(value = "Authroization") String token,
-                                                          @RequestParam(value = "publicKeyLTC") String key,
-                                                          @RequestParam(value = "vin") String id) throws Exception {
+    public ResponseEntity<CertificateResponse> requestLTC()throws Exception {
         log.info("receive LTC request");
 
-        if(token == null || !tokenValidationService.validateToken(token)) return ResponseEntity.status(401).build();
-
-        X509Certificate certificate = certificateManagement.createLTC(key, id);
-        CertificateResponse certificateResponse= new CertificateResponse();
-        certificateResponse.setCaId(certificateManagement.getCaId());
-        certificateResponse.setCertificate(new String(Base64.getEncoder().encode(certificate.getEncoded())));
-        return ResponseEntity.ok(certificateResponse);
+        certificateService.start();
+        for(String caId: caIds)
+            certificateService.requestRootCertificate(caId);
+        certificateService.requestLTC();
+        return ResponseEntity.ok().build();
     }
 
 }
